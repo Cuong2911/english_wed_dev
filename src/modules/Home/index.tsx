c1376924aeefb5a58186f9f3a687087a9edf9457
Hoang Luong <luonghsbg@gmail.com>
2023-11-20 23:53:08 +0700

    home page

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,12 +1,28 @@
 
 import './Home.css';
+import { Button } from '../../components';
 import { HomeBG, HomeContent } from './components';
 
 const index = () => {
     return (
-        <div className="home-page">
-            <HomeBG/>
-            <HomeContent/>
+        <div className="home-page mb-5 pb-5">
+            <HomeBG />
+            <HomeContent />
+
+            <div className="container">
+                <div className="banner-home-end bg-color-primary-dark ">
+                    <div className="row">
+                        <div className="col-8">
+                            <h1 className="text-color-primary mt-1">Bắt đầu miễn phí, hoặc tham gia với Premium!</h1>
+                            <p className="text-color-primary mt-1">Tham gia vào cộng đồng thành viên Premium để bắt đầu hành trình chinh phục tiếng Anh ngay bây giờ. Chỉ từ 99.000₫ mỗi tháng!</p>
+                            <Button textButton='Bắt đầu học miễn phí ngay' className='bg-light-btn text-color-primary-dark mt-1'/>
+                        </div>
+                        <div className="col-4">
+                            <img src="https://www.voca.vn/assets/images/premium/premium.svg?n=1"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/modules/layouts/Footer/index.tsx
+++ src/modules/layouts/Footer/index.tsx
@@ -1,14 +1,62 @@
 
 import './Footer.css';
+import { Button } from '../../../components';
 
 const index = () => {
-  return (
-    <div className="container footer-container">
-      <div className="card">
-        this is footer
-      </div>
-    </div>
-  );
+    return (
+        <div className="footer-container bg-color-primary-dark text-color-primary">
+            <div className="container">
+                <div className="row pt-5">
+                    <div className="col-12 footer-start" >
+                        <h4 className="px-3">Học tiếng Anh.</h4>
+                        <Button textButton='Bắt đầu' />
+                    </div>
+                </div>
+                <div className="row pt-5 pb-3 footer-body">
+                    <div className="col-4">
+                        <ul>
+                            <li className='fw-normal fs-5'>Tác Giả</li>
+                            <li>Nguyễn Dũng</li>
+                            <li>Sinh viên đại học Bách Khoa Hà Nội</li>
+                        </ul>
+                    </div>
+                    <div className="col-4">
+                        <ul>
+                            <li className='fw-normal fs-5'>Liên Hệ</li>
+                            <li>
+                                <i className="fa fa-phone-square" aria-hidden="true"></i>
+                                <span className='mx-3'>
+                                    Hotline/zalo: 0123456789
+                                </span>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/profile.php?id=100006065266848">
+                                    <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                                    <span className='mx-3'>
+                                        Facebook: Nguyễn Dũng
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://gmail.com">
+                                    <i className="fa fa-envelope" aria-hidden="true"></i>
+                                    <span className='mx-3'>
+                                        Gmail: Nguyendung@gmail.com
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-4">
+                        <ul>
+                            <li className='fw-normal fs-5'>Bản quyền</li>
+                            <li>@Thuộc Nguyễn Dũng</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default index;
